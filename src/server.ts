e7cf7e990d1cbc0e15eff6d92a3c8939ef567943
GodKelvin <kelvin.lehrback@gmail.com>
2023-06-22 21:35:30 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+
+import express, { Request, Response } from 'express';
+//import indexRoutes from './routes/indexRoutes';
+import dotenv from 'dotenv';
+//import cron from 'node-cron';
+
+//Carregando variaveis de ambiente
+dotenv.config();
+
+const server = express();
+//server.use(indexRoutes);
+
+server.get('/', async(_req: Request, res: Response) => {
+    res.status(200).json({"Feito por: Kelvin Lehrback":"-> https://github.com/GodKelvin"});
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Servidor running on port ${port}, acess: http://localhost:${port}/`);
+});
